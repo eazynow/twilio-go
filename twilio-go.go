ab595bf27495dcbe5e2c54439d95e05305d527b4
Erol Ziya <erol.ziya@gmail.com>
2013-05-14 07:46:08 +0100

    wip creating client

---
+++ twilio-go.go
@@ -0,0 +1,31 @@
+package twilio-go
+
+// add a constant for version
+
+type TwilioRestClient struct {
+	AccountSid string
+	AuthTokenredentials string
+	Endpoint string
+	DefaultVersion string
+	NumRetries int
+
+}
+
+func (twilio *Twilio) post(formValues url.Values, twilioUrl string) (*http.Response, error) {
+	req, err := http.NewRequest("POST", twilioUrl, strings.NewReader(formValues.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	
+	// use basic auth to connect
+	req.SetBasicAuth(twilio.AccountSid, twilio.AuthToken)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	c := &http.Client{}
+	return c.Do(req)
+}
+
+func NewTwilioRestClient(accountSid, authToken, endPoint string){
+
+	trc := &TwilioRestClient{accountSid, authToken, endPoint}
+}
